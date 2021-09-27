aa0710a5c53e0b5601b2cd77f629fc0ea24ef7a8
wilsonns <30836552+wilsonns@users.noreply.github.com>
2021-09-27 12:29:14 -0300

    Create item.py

---
+++ item.py
@@ -0,0 +1,15 @@
+class item(object):
+	def __init__(self,x,y):
+    self.x = x
+    self.y = y
+    self.nome = ""
+    self.descricao = ""
+    self.sprite = None
+    self.tipo = ""
+    self.conteudo = []
+    self.dano = 0
+    self.iniciativa = 0
+    
+class arma(item):
+	def __super__.init():
+		pass
